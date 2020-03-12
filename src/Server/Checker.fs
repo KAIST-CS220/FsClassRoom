29c9c935ac489b6eefe2a104a93d4519c8ba079e
Sang Kil Cha <sangkilc@kaist.ac.kr>
2020-03-12 12:06:31 +0900

    Show better error

--- src/Server/Checker.fs
+++ src/Server/Checker.fs
@@ -4,18 +4,29 @@
 module FsClassroom.Checker
 
 open System.Reflection
+open System.Collections.Generic
 
 let check (asm: Assembly) method =
   let mutable total = 0
   let mutable succeed = 0
+  let msgqueue = Queue<string> ()
   try
     for t in asm.GetTypes () do
       for m in t.GetMethods () do
         if m.Name.StartsWith "test" then
           total <- total + 1
+          msgqueue.Enqueue ("[" + total.ToString () + "] testing " + m.Name)
           if m.Invoke (null, [| method |]) |> unbox<bool> then
+            msgqueue.Enqueue ("Success!")
             succeed <- succeed + 1
-          else ()
+          else
+            msgqueue.Enqueue ("Failed.")
         else ()
-    sprintf "%d / %d complete.\n" succeed total
-  with msg -> msg.Message + "\n"
+    msgqueue.Enqueue (
+      succeed.ToString () + "/" + total.ToString () + " completed.")
+    String.concat "\n" msgqueue
+  with msg ->
+    "[Error] " + msg.Message + "\n\n\
+     This means your file cannot be compiled, or\n\
+     your function has a wrong signature (parameter types are wrong).\n\n"
+    + "[Log]\n\n" + String.concat "\n" msgqueue + "\n"
